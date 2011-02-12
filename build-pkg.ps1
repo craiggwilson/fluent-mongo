78ef4050470f799934de0a421ca8611662e3fa34
rhwy <git@rui.fr>
2011-02-12 01:16:12 +0100

    updated manifest to take local bin drivers/bson, update build script to create PackageRelease folder is not exists

--- build-pkg.ps1
+++ build-pkg.ps1
@@ -2,5 +2,8 @@ $currentPath =  $myinvocation.mycommand.path | split-path -parent
 $currentPath
 push-location
 set-location $currentPath
+if(-not (Test-Path ".\PackageRelease")) {
+	New-Item "PackageRelease" -type "Directory"
+}
 .\Tools\NuGet.exe pack "$currentPath\FluentMongo.nuspec" -o ".\PackageRelease\"
 pop-location
